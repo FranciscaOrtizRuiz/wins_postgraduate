dc66f78a5b6f5fe6c16d0f8245f3e8ead698bb8f
FranciscaOrtizRuiz <franortizruiz@gmail.com>
2021-04-08 22:50:09 +0100

    add log

---
+++ imag/sticker.R
@@ -0,0 +1,26 @@
+
+### Sticker ###
+
+library(hexSticker)
+
+g<-"imag/log.jpg"
+
+sticker(g,package="",
+        p_size=3.0, #size text
+        p_x = 1, # x position text
+        p_y = 1.4, # y position text
+        p_color = "black", # colour text
+        p_family = "gloria", #Aller_Rg
+        #spotlight=TRUE,
+        #u_size=0.9,
+        s_x=1.0,  # x centering
+        s_y=1.0, # y centering
+        s_width=0.5,
+        s_height=0.5,
+        h_fill="#FFFFFF", 
+        h_color="#A9A9A9", filename="imag/sticker.jpg", 
+        asp = 1.0, dpi = 1000
+)
+
+
+
